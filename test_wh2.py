e5b1f3db32ce9772bfe309b7d39221b52f80ded6
Artem09076 <grigoranartem592@yandex.ru>
2023-10-04 15:45:21 +0300

    add files

--- test_wh2.py
+++ test_wh2.py
@@ -1,10 +1,10 @@
-"""Test for task2."""
+"""Test for test_wh2."""
 
 import pytest
 
-from task2 import degree
+from ball.task2 import degree
 
-test_cases = (
+TEST_CASES = (
     (5.0, 2.0, 3.0, 4.0, 0)
 ), (
     (5.0, 1.0, 3.0, 4.0, 180)
@@ -13,7 +13,7 @@ test_cases = (
 )
 
 
-@pytest.mark.parametrize('radius, accel, time, velocity, expected', test_cases)
+@pytest.mark.parametrize('radius, accel, time, velocity, expected', TEST_CASES)
 def test_degree(radius: float, accel: float, time: float, velocity: float, expected: int) -> None:
     """Test our degree function.
 
